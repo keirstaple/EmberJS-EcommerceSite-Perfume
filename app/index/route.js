205fdd4c891fe57f3d742193e3b8b59d9271e0ba
keir staple and paul hess <keirstaple@comcast.net>
2016-04-07 08:21:41 -0700

    add sign out functionality

--- app/index/route.js
+++ app/index/route.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findAll('perfume');
+  },
+
+  actions: {
+    signOut: function() {
+      this.get("session").close();
+    }
   }
 });
