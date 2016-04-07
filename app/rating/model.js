0b4e34b6e60a4b7e1d169c1b021f492133012bf0
keir staple and paul hess <pauljacksonhess@gmail.com>
2016-04-07 14:32:38 -0700

    fix bug with rating by changing the rating model

--- app/rating/model.js
+++ app/rating/model.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  value: DS.belongsTo('perfume', {async: true})
+  value: DS.attr(),
+  perfume: DS.belongsTo('perfume', {async: true})
 });
